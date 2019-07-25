9df2227aea837583811ec3a96536255cde668e38
Leon Oldfritter <leon.zcf@gmail.com>
2019-07-26 06:07:41 +0800

    简化配置

--- config/env.go
+++ config/env.go
@@ -10,50 +10,12 @@ import (
 
 type Env struct {
 	Model string `yaml:"model"`
-
-	Matching struct {
-		Node string `yaml:"node"`
-	} `yaml:"matching"`
-
-	TradeTreat struct {
-		Node string `yaml:"node"`
-	} `yaml:"trade_treat"`
-
-	OrderCancel struct {
-		Node string `yaml:"node"`
-	} `yaml:"order_cancel"`
+	Node  string `yaml:"node"`
 
 	Newrelic struct {
 		AppName    string `yaml:"app_name"`
 		LicenseKey string `yaml:"license_key"`
 	} `yaml:"newrelic"`
-
-	Host string `yaml:"HOST"`
-
-	Email struct {
-		SmtpPort       int    `yaml:"SMTP_PORT"`
-		SmtpDomain     string `yaml:"SMTP_DOMAIN"`
-		SmtpAddress    string `yaml:"SMTP_ADDRESS"`
-		SmtpUsername   string `yaml:"SMTP_USERNAME"`
-		SmtpPassword   string `yaml:"SMTP_PASSWORD"`
-		SystemMailFrom string `yaml:"SYSTEM_MAIL_FROM"`
-	} `yaml:"EMAIL"`
-
-	Sms struct {
-		SmsUser        string `yaml:"SMS_USER"`
-		SmsKey         string `yaml:"SMS_KEY"`
-		ValidateCode   string `yaml:"VALIDATE_CODE"`
-		ApiUrl         string `yaml:"API_URL"`
-		Sms253Account  string `yaml:"SMS_253_ACCOUNT"`
-		Sms253Password string `yaml:"SMS_253_PASSWORD"`
-		VoiceApiUrl    string `yaml:"VOICE_API_URL"`
-	} `yaml:"SMS"`
-
-	GarnetClient struct {
-		ApiBaseUrl  string `yaml:"ApiBaseUrl"`
-		TenantId    string `yaml:"TenantId"`
-		TenantToken string `yaml:"TenantToken"`
-	} `yaml:"GarnetClient"`
 }
 
 var CurrentEnv Env
